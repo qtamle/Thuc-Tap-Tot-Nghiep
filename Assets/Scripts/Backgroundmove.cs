4ccf369b5cdd64f8507d1a88f75ca0ac90371dc7
BennyPhan <160525636+phuongphan1103@users.noreply.github.com>
2025-03-26 19:22:46 +0700

    login background

---
+++ Assets/Scripts/Backgroundmove.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Backgroundmove : MonoBehaviour
+{
+    Material mat;
+    float distance;
+
+    [Range(0f,0.5f)]
+    public float speed=0.2f;
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+    void Update()
+    {
+        distance += Time.deltaTime*speed;
+        mat.SetTextureOffset("_MainTex", Vector2.right * distance);
+    }
+}
