c23317e6cf61166405da023e65e1d0c784024d6a
BennyPhan <160525636+phuongphan1103@users.noreply.github.com>
2025-04-01 17:36:03 +0700

    sao no khong chuyen nhac vay?

--- Assets/Scripts/AudioScript/PersistentMusic.cs
+++ Assets/Scripts/AudioScript/PersistentMusic.cs
@@ -3,24 +3,34 @@ using UnityEngine;
 public class PersistentMusic : MonoBehaviour
 {
     private static PersistentMusic instance;
-    private AudioSource audioSource;
 
     void Awake()
     {
+        AudioSource existingAudio = FindObjectOfType<AudioSource>(); 
+
+        if (existingAudio != null && existingAudio.gameObject != gameObject)
+        {
+            Destroy(gameObject); 
+            return;
+        }
+
         if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
 
-            audioSource = GetComponent<AudioSource>(); 
-            audioSource.loop = true; 
-            audioSource.playOnAwake = true;
-            audioSource.volume = 0.6f; // Điều chỉnh vol, trên máy t là nghe 0.6 là vừa, có gì chỉnh lại trên máy bây nha
-            audioSource.Play(); 
+            AudioSource audioSource = GetComponent<AudioSource>(); 
+            if (audioSource != null)
+            {
+                audioSource.loop = true; 
+                audioSource.playOnAwake = true;
+                audioSource.volume = 0.6f;
+                audioSource.Play();
+            }
         }
         else
         {
-            Destroy(gameObject);
+            Destroy(gameObject); 
         }
     }
 }
