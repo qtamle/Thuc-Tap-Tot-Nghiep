f5657fe3935289960e485e6f0ef88d96d184c314
EyelessJack0903 <eyelessjack0903@gmail.com>
2025-04-02 13:35:11 +0700

    z

--- Assets/Scripts/Coins_Exps/ExperienceManager.cs
+++ Assets/Scripts/Coins_Exps/ExperienceManager.cs
@@ -55,6 +55,19 @@ public class ExperienceManager : MonoBehaviour
         UpdateExperienceUI();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            AddExperience(1000);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SceneManager.LoadScene("Summary");
+        }
+    }
+
     public void AddExperience(int amount)
     {
         experienceCount += amount;
