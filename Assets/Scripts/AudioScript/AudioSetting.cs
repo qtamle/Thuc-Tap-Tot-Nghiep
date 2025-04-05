ed6d9e93eadcb71829d66cadce89f988ddffd747
BennyPhan <160525636+phuongphan1103@users.noreply.github.com>
2025-04-05 23:11:18 +0700

    fix dùm Phương cái Toggle đi
    Phương đổi hàm text trong code thành TMP rùi nhma nó vẫn ko nhận, cay thậc

--- Assets/Scripts/AudioScript/AudioSetting.cs
+++ Assets/Scripts/AudioScript/AudioSetting.cs
@@ -1,33 +1,50 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class AudioSettings : MonoBehaviour
 {
     [SerializeField] private Toggle musicToggle;
     [SerializeField] private Toggle sfxToggle;
 
+    private TextMeshProUGUI musicToggleText;
+    private TextMeshProUGUI sfxToggleText;
+
     private void Start()
     {
+        musicToggleText = musicToggle.transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>();
+        sfxToggleText = sfxToggle.transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>();
+
         musicToggle.isOn = MusicHandler.instance != null ? !MusicHandler.instance.GetComponent<AudioSource>().mute : true;
         sfxToggle.isOn = SFXHandler.instance != null ? !SFXHandler.instance.GetComponent<AudioSource>().mute : true;
+
+        UpdateToggleText(musicToggle.isOn, musicToggleText);
+        UpdateToggleText(sfxToggle.isOn, sfxToggleText);
 
         musicToggle.onValueChanged.AddListener(ToggleMusic);
         sfxToggle.onValueChanged.AddListener(ToggleSFX);
     }
 
-    private void ToggleMusic(bool isOn)
+    public void ToggleMusic(bool isOn)
     {
         if (MusicHandler.instance != null)
         {
             MusicHandler.instance.ToggleMusic(isOn);
+            UpdateToggleText(isOn, musicToggleText);
         }
     }
 
-    private void ToggleSFX(bool isOn)
+    public void ToggleSFX(bool isOn)
     {
         if (SFXHandler.instance != null)
         {
             SFXHandler.instance.ToggleSFX(isOn);
+            UpdateToggleText(isOn, sfxToggleText);
         }
+    }
+
+    private void UpdateToggleText(bool isOn, TextMeshProUGUI toggleText)
+    {
+        toggleText.text = isOn ? "ON" : "OFF";
     }
 }
