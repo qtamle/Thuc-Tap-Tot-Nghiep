9586fba52dc0cee67b62998972e4aa21c924be76
BennyPhan <160525636+phuongphan1103@users.noreply.github.com>
2025-04-05 17:31:29 +0700

    ads, âm thanh, xong rồi

---
+++ Assets/Scripts/UnityIntersitial.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class UnityInterstitial : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    public string gameID = "";
+    public string intersitialID = "";
+    [SerializeField] bool _testMode = true;
+
+
+
+    void Start()
+    {
+        InitializeAds();
+        LoadAd();
+    }
+
+   public void InitializeAds()
+    {
+        if (!Advertisement.isInitialized && Advertisement.isSupported)
+        {
+            Advertisement.Initialize(gameID, _testMode, this);
+        }
+    }
+
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete.");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+    }
+
+    public void LoadAd()
+    {
+        Advertisement.Load(intersitialID, this);
+    }
+
+    // Show the loaded content in the Ad Unit:
+    public void ShowAd()
+    {
+        Advertisement.Show(intersitialID, this);
+    }
+
+    // Implement Load Listener and Show Listener interface methods:
+    public void OnUnityAdsAdLoaded(string adUnitId)
+    {
+        // Optionally execute code if the Ad Unit successfully loads content.
+    }
+
+    public void OnUnityAdsFailedToLoad(string _adUnitId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Error loading Ad Unit: {_adUnitId} - {error.ToString()} - {message}");
+        // Optionally execute code if the Ad Unit fails to load, such as attempting to try again.
+        LoadAd();
+    }
+
+    public void OnUnityAdsShowFailure(string _adUnitId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"Error showing Ad Unit {_adUnitId}: {error.ToString()} - {message}");
+        // Optionally execute code if the Ad Unit fails to show, such as loading another ad.
+        LoadAd();
+    }
+
+    public void OnUnityAdsShowStart(string _adUnitId) { }
+    public void OnUnityAdsShowClick(string _adUnitId) { }
+    public void OnUnityAdsShowComplete(string _adUnitId, UnityAdsShowCompletionState showCompletionState) { }
+}
